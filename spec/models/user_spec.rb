9bc7b84dd28a010fd5ecf179384807a4c18ed0e5
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-09-30 18:09:53 +0530

    install rspec and add spec file for test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+# spec/models/user_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with valid attributes" do
+    user = User.new(username: "example", email: "user@example.com")
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a username" do
+    user = User.new(email: "user@example.com")
+    expect(user).not_to be_valid
+  end
+end
