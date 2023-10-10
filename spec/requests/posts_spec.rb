e5db47a3ee0729a2d5027bd232c48346eb8e727c
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-10-10 21:52:57 +0530

    add spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users/:user_id/posts' do
+    let(:user) { User.create(name: 'Test User') }
+
+    before do
+      get "/users/#{user.id}/posts"
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the list of posts for the selected user in the response body' do
+      expect(response.body).to include('List of post for selected User')
+    end
+  end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    let(:user) { User.create(name: 'Test User') }
+    let(:post) { user.posts.create(title: 'Test Post', text: 'Test Content') }
+
+    before do
+      get "/users/#{user.id}/posts/#{post.id}"
+    end
+
+    it 'returns a successful response' do
+        expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the selected post title in the response body' do
+      expect(response.body).to include('Selected post for a user')
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users' do
+    before do
+      User.create(name: 'Test User')
+      get users_path
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the user index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the list of users in the response body' do
+      expect(response.body).to include('List of Users')
+    end
+  end
+
+  describe 'GET /users/:id' do
+    let(:user) { User.create(name: 'Test User') }
+
+    before do
+      get user_path(user)
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the selected user name in the response body' do
+      expect(response.body).to include('Selected User')
+    end
   end
 end
